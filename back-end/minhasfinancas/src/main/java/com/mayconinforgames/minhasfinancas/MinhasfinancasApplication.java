7582d258169d299966ec8b6ea2ab318ff9aea65f
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-06-30 19:50:35 -0300

    renomeando o nome do pacote do projeto

--- back-end/minhasfinancas/src/main/java/com/mayconinforgames/minhasfinancas/MinhasfinancasApplication.java
+++ back-end/minhasfinancas/src/main/java/com/mayconinforgames/minhasfinancas/MinhasfinancasApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.mayconinforgames.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
