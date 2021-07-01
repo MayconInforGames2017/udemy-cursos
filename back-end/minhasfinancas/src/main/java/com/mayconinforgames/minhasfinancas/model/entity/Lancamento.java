57b7d9244def2948858ff0429743c9bd8e9e397a
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-06-30 21:22:45 -0300

    criando a classe enum tipolancamento

--- back-end/minhasfinancas/src/main/java/com/mayconinforgames/minhasfinancas/model/entity/Lancamento.java
+++ back-end/minhasfinancas/src/main/java/com/mayconinforgames/minhasfinancas/model/entity/Lancamento.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -14,6 +16,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import com.mayconinforgames.minhasfinancas.model.enuns.TipoLancamento;
 
 @Entity
 @Table(name = "lancamento")
@@ -41,5 +45,9 @@ public class Lancamento {
 	@Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
 	private LocalDate dataCadastro;
 
+	@Column(name = "tipo")
+	@Enumerated(value = EnumType.STRING)
+	private TipoLancamento tipo;
+	
 	
 }
