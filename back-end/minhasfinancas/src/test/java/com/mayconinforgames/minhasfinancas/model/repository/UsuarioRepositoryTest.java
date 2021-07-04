ed30869f4d368dd13ac4daca3a8fdab157dad736
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-03 22:44:38 -0300

    alterando as dependencias de testes no pom.xml

--- back-end/minhasfinancas/src/test/java/com/mayconinforgames/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ back-end/minhasfinancas/src/test/java/com/mayconinforgames/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,16 +1,19 @@
 package com.mayconinforgames.minhasfinancas.model.repository;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.mayconinforgames.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+
 public class UsuarioRepositoryTest {
 
 	@Autowired
