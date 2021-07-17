b43e551b506992ba8ee0940c96e6a8fd5b2f90c4
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-17 20:49:30 -0300

    Refatorando as entidades adicionando as anotacoes lombok

--- backend/financas/src/main/java/br/mayconinforgames/minhasfinancas/model/entity/Lancamentos.java
+++ backend/financas/src/main/java/br/mayconinforgames/minhasfinancas/model/entity/Lancamentos.java
@@ -2,7 +2,6 @@ package br.mayconinforgames.minhasfinancas.model.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
@@ -20,9 +19,23 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import br.mayconinforgames.minhasfinancas.model.enuns.StatusLancamento;
 import br.mayconinforgames.minhasfinancas.model.enuns.TipoLancamento;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "lancamento", schema = "financas")
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Lancamentos {
 
 	@Id
@@ -57,104 +70,5 @@ public class Lancamentos {
 	@Column(name = "status")
 	@Enumerated(value = EnumType.STRING)
 	private StatusLancamento status;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Integer getMes() {
-		return mes;
-	}
-
-	public void setMes(Integer mes) {
-		this.mes = mes;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public TipoLancamento getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(TipoLancamento tipo) {
-		this.tipo = tipo;
-	}
-
-	public StatusLancamento getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusLancamento status) {
-		this.status = status;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(ano, dataCadastro, descricao, id, mes, status, tipo, usuario, valor);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lancamentos other = (Lancamentos) obj;
-		return Objects.equals(ano, other.ano) && Objects.equals(dataCadastro, other.dataCadastro)
-				&& Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
-				&& Objects.equals(mes, other.mes) && status == other.status && tipo == other.tipo
-				&& Objects.equals(usuario, other.usuario) && Objects.equals(valor, other.valor);
-	}
-
-	@Override
-	public String toString() {
-		return "Lancamentos [id=" + id + ", descricao=" + descricao + ", mes=" + mes + ", ano=" + ano + ", usuario="
-				+ usuario + ", valor=" + valor + ", dataCadastro=" + dataCadastro + ", tipo=" + tipo + ", status="
-				+ status + "]";
-	}
 
 }
--- backend/financas/src/main/java/br/mayconinforgames/minhasfinancas/model/entity/Usuario.java
+++ backend/financas/src/main/java/br/mayconinforgames/minhasfinancas/model/entity/Usuario.java
@@ -1,7 +1,5 @@
 package br.mayconinforgames.minhasfinancas.model.entity;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,8 +7,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Entity
 @Table(name = "usuario", schema = "financas")
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuario {
 
 	@Id
@@ -26,60 +39,5 @@ public class Usuario {
 
 	@Column(name = "senha")
 	private String senha;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(email, id, nome, senha);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
-				&& Objects.equals(senha, other.senha);
-	}
-
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + "]";
-	}
 
 }
