974b790335a4a3eaf6d068c3217ee7528b832d1d
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-17 18:49:12 -0300

    criando o backend projeto

---
+++ backend/financas/src/test/java/br/mayconinforgames/minhasfinancas/FinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mayconinforgames.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
