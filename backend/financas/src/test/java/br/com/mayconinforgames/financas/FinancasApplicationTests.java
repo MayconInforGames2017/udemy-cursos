0222a80e41638455717ef2485252c92cec2d4d10
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-16 22:45:46 -0300

    alterando a versao do projeto

--- backend/financas/src/test/java/br/com/mayconinforgames/financas/FinancasApplicationTests.java
+++ backend/financas/src/test/java/br/com/mayconinforgames/financas/FinancasApplicationTests.java
@@ -1,13 +1,5 @@
 package br.com.mayconinforgames.financas;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class FinancasApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
