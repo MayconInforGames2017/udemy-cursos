5e269de10894de2ced79de56312c39390756b749
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-18 15:06:24 -0300

    implementando teste de validar email no servico de usuario

---
+++ backend/financas/src/test/java/br/com/mayconinforgames/minhasfinancas/service/UsuarioServiceTest.java
@@ -0,0 +1,47 @@
+package br.com.mayconinforgames.minhasfinancas.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.mayconinforgames.minhasfinancas.exception.RegraNegocioException;
+import br.com.mayconinforgames.minhasfinancas.model.entity.Usuario;
+import br.com.mayconinforgames.minhasfinancas.model.repository.UsuarioRepository;
+import br.com.mayconinforgames.minhasfinancas.service.UsuarioService;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class UsuarioServiceTest {
+
+	@Autowired
+	UsuarioService usuarioService;
+	
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	@Test(expected = Test.None.class)
+	public void deveValidarEmail() {
+		// cenario
+		usuarioRepository.deleteAll();
+		
+		// ação
+		usuarioService.validarEmail("email@email.com");
+			
+	}
+	
+	@Test(expected = RegraNegocioException.class)
+	public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
+		// cenario
+		Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
+		usuarioRepository.save(usuario);
+		
+		// ação
+		usuarioService.validarEmail("email@email.com");
+		
+	}
+	
+}
