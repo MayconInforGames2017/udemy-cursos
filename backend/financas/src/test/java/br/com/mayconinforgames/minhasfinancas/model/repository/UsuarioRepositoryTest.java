6bd676d1789e581606b8a5c89889721c9f0b7dde
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-18 15:28:19 -0300

    melhorando os testes de integracao com @datajpatest

--- backend/financas/src/test/java/br/com/mayconinforgames/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ backend/financas/src/test/java/br/com/mayconinforgames/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,20 +1,21 @@
 package br.com.mayconinforgames.minhasfinancas.model.repository;
 
 import org.assertj.core.api.Assertions;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import br.com.mayconinforgames.minhasfinancas.model.entity.Usuario;
 
-@SpringBootTest
+@DataJpaTest 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UsuarioRepositoryTest {
 
 	@Autowired
